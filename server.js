872d1fa59192df594c5246c2216db109f00eee31
mtorcellini <matt.torcellini@gmail.com>
2020-12-16 13:39:23 -0500

    add html routes

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const PORT = 3000;
 const app = express();
 app.use(logger("dev")); // use morgan for logging
 app.use(express.static("public")); // serve public folder
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+// ----------------------------------------------------- 
+// html routes
+app.get('/exercise', (req, res) => {
+    res.sendFile(__dirname + '/public/exercise.html');
+});
+
+app.get('/stats', (req, res) => {
+    res.sendFile(__dirname + '/public/stats.html');
+});
+// ----------------------------------------------------- 
+
 
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
