c025b380873dcc9774f6b9f99f425b7f115411e6
mtorcellini <matt.torcellini@gmail.com>
2020-12-16 16:27:08 -0500

    add functionality for creating new workouts and displaying data in dashboard

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const logger = require('morgan');
+const mongojs = require('mongojs');
+
+
+mongoose.connect( "mongodb://localhost/workout", {useNewUrlParser: true, useUnifiedTopology: true} );
+const db = require('./models');
 
 const PORT = 3000;
 
@@ -20,6 +25,34 @@ app.get('/stats', (req, res) => {
     res.sendFile(__dirname + '/public/stats.html');
 });
 // ----------------------------------------------------- 
+
+// -----------------------------------------------------
+// api routes
+
+// workout dashboard
+app.get('/api/workouts/range', (req, res) => {
+    db.Workout.find().then( (result) => {
+        res.json(result);
+    });
+});
+
+// new workout
+app.post('/api/workouts/', (req, res) => {
+    db.Workout.create({}).then( (result) => {
+        console.log(result);
+        res.json(result);
+    });
+});
+
+// update workout
+app.put('/api/workouts/:id', (req, res) => {
+    let idToUpdate = req.params.id;
+    db.Workout.updateOne({_id : mongojs.ObjectID(idToUpdate)}, {$push : {exercises : req.body}}, (err, result) => {
+        res.json(result);
+    });
+});
+
+// -----------------------------------------------------
 
 
 app.listen(PORT, () => {
